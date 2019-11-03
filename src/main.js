f9e47b2e2c719d236c59b616cbecdbdd8997b5e4
Toby56 <Toby56@pm.me>
2019-11-04 09:27:40 +1100

    Add a package.json and modify main.js so the app actually works like the Electron quick start app!

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ function createWindow () {
     });
 
     // and load the index.html of the app.
-    mainWindow.loadFile('index.html');
+    mainWindow.loadFile('src/index.html');
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
