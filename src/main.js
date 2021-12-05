88d5b0b973e9c1761f5b6d69daeb2ca8db45fcf4
Linux Snoot <49733954+NovaAppsInc@users.noreply.github.com>
2021-12-05 16:34:43 -0500

    Fixed window control
    Removed all "remote" instances and added ipc instances

--- src/main.js
+++ src/main.js
@@ -1,19 +1,41 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path');
+const ipc = ipcMain;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         frame: false,
-        backgroundColor: '#FFF',
+        backgroundColor: '#ffffff',
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false,
+            devTools: false,
+            preload: path.join(__dirname, 'preload.js'),
+        },
+        icon: "icon source here"
+    });
+
+    ipc.on("closeApp", () => {
+        mainWindow.close();
+    });
+    ipc.on("minApp", () => {
+        mainWindow.minimize();
+    });
+    ipc.on("maxApp", () => {
+        if(mainWindow.isMaximized()) {
+            mainWindow.webContents.send("changeIr");
+            mainWindow.restore();
+        } else {
+            mainWindow.webContents.send("changeImx");
+            mainWindow.maximize();
         }
     });
 
@@ -25,10 +47,10 @@ function createWindow () {
 
     // Emitted when the window is closed.
     mainWindow.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
     });
 }
 
@@ -38,7 +60,7 @@ function createWindow () {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
@@ -46,7 +68,7 @@ app.on('window-all-closed', function () {
     }
 });
 
-app.on('activate', function () {
+app.on('activate', function() {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
